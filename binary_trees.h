9b51a60a2b1f31ad8bc5a44d509d5fe0a1b70df9
NaiM_AyouB <ayoubnaim833@gmail.com>
2024-01-29 20:12:51 +0100

    add function that  left-child to header file

--- binary_trees.h
+++ binary_trees.h
@@ -34,4 +34,7 @@ void binary_tree_print(const binary_tree_t *);
 /* task 0. New node */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
+/* task 1.  Insert left*/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
 #endif /* _BINARY_TREES_H_ */
