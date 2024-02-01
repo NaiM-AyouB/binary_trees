e3cbd87fa4974aaf27b4f21e8c5aa172ddd1fb83
Anas Asimi <anas2020cr7@gmail.com>
2024-02-01 23:02:23 +0100

    Complete task 20. Level-order traversal

--- binary_trees.h
+++ binary_trees.h
@@ -89,4 +89,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 /* task 19. Lowest common ancestor */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 
+/* task 20. Level-order traversal */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+
 #endif /* _BINARY_TREES_H_ */
