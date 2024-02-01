dc271e0a0697837b34798879b77544b7daa215e5
NaiM_AyouB <ayoubnaim833@gmail.com>
2024-02-01 01:09:12 +0100

    last try

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,14 +10,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 	{
-		size_t left_height;
-		size_t right_height;
+		size_t left_height = 0;
+		size_t right_height = 0;
 
 		left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
 		return ((left_height > right_height) ? left_height : right_height);
 	}
-	
+
 	return (0);
 }
