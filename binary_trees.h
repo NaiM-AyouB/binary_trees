25b2eb0c3d2bb11252adfd91cce8f90679d06e50
Anas Asimi <anas2020cr7@gmail.com>
2024-02-02 22:06:44 +0100

    Complete task 22. Rotate left

--- binary_trees.h
+++ binary_trees.h
@@ -95,4 +95,7 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 /* task 20. Level-order traversal */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 
+/* task 22. Rotate left */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
+
 #endif /* _BINARY_TREES_H_ */
